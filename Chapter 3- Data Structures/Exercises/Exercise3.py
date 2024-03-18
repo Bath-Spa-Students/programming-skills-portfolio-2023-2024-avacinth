7e6f0bca0fd0c6d22910b0c2cb845e0543d33a8f
avacinth <toribiohyacinth05@gmail.com>
2024-03-19 00:27:08 +0400

    Update Exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,13 @@
+# Create a List
+
+modes_of_transportation = ["Car", "Motorcycle", "Bicycle", "Scooter"]
+
+# Printing the statements about each items in the list
+
+print (f"I would like to own a", modes_of_transportation[0])
+
+print (f"I've always wanted a", modes_of_transportation[1])
+
+print (f"Riding a", modes_of_transportation[2], "is a great way to stay fit.")
+
+print (modes_of_transportation[3], "is perfect for city commuting.")
