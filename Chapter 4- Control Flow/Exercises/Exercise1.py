06db060cca484553245fff98b9029c08b2d4c632
avacinth <toribiohyacinth05@gmail.com>
2024-03-19 01:39:26 +0400

    Update Exercise1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,17 @@
+# String Variable
+
+alien_color = 'green'
+
+# If Statement
+
+if alien_color == 'green':
+    print ("Congratulations, you just earned 5 points!")
+
+# Failing Version
+
+alien_color = 'yellow'
+
+# If Statement
+
+if alien_color == 'green':
+    print ("Congratulations, you just earned 5 points!")
