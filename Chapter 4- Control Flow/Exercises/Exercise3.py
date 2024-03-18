de236fb6d6b99a48ad3f577d8a74150238209928
avacinth <toribiohyacinth05@gmail.com>
2024-03-19 01:39:34 +0400

    Update Exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise3.py
@@ -0,0 +1,41 @@
+''' Green Alien Color
+String Variable '''
+
+alien_color = 'green'
+
+# If-elif-else statement
+
+if alien_color == 'green':
+    print ("Congratulations, you just earned 5 points!")
+elif alien_color == 'yellow':
+    print ("Congratulations, you just earned 10 points!")
+else:
+    print ("Congratulations, you just earned 15 points!")
+
+''' Yellow Alien Color
+String Variable '''
+
+alien_color = 'yellow'
+
+# If-elif-else statement
+
+if alien_color == 'green':
+    print ("Congratulations, you just earned 5 points!")
+elif alien_color == 'yellow':
+    print ("Congratulations, you just earned 10 points!")
+else:
+    print ("Congratulations, you just earned 15 points!")
+
+''' Red Alien Color
+String Variable '''
+
+alien_color = 'red'
+
+# If-elif-else statement
+
+if alien_color == 'green':
+    print ("Congratulations, you just earned 5 points!")
+elif alien_color == 'yellow':
+    print ("Congratulations, you just earned 10 points!")
+else:
+    print ("Congratulations, you just earned 15 points!")
