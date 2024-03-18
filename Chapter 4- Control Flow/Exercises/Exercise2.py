da3873ce89e0e2565daa3c6d5e2b5a5e8368a5f6
avacinth <toribiohyacinth05@gmail.com>
2024-03-19 01:39:31 +0400

    Update Exercise2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -0,0 +1,23 @@
+''' If statement version
+String Variable '''
+
+alien_color = 'green'
+
+# If-else statement
+
+if alien_color == 'green':
+    print ("Congratulations, you just earned 5 points!")
+else:
+    print ("Congratulations, you just earned 10 points!")
+
+''' Else statement version
+String Variable '''
+
+alien_color = 'red'
+
+# If-else statement
+
+if alien_color == 'green':
+    print ("Congratulations, you just earned 5 points!")
+else:
+    print ("Congratulations, you just earned 10 points!")
