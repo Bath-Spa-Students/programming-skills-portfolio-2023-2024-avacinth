5a5bdcecac92c7bba74325344ee08818710fc559
avacinth <toribiohyacinth05@gmail.com>
2024-03-19 00:27:11 +0400

    Update Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,10 @@
+''' Guest List
+List of inviting people to dinner '''
+guests_name = ['Jennie Kim', 'Rose Park', 'Jisoo Kim']
+
+# Print each list index and message
+print (guests_name[0]+ ", I would like to invite you to dinner.")
+
+print (guests_name[1]+ ", I would like to invite you to dinner.")
+
+print (guests_name[2]+ ", I would like to invite you to dinner.")
