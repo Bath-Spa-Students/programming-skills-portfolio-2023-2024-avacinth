37d56e45484256540ea503d818dbf680b1f7edcf
avacinth <toribiohyacinth05@gmail.com>
2024-03-19 00:27:24 +0400

    Update Exercise7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise7.py
@@ -0,0 +1,35 @@
+# Seeing the World
+
+# Creating a list of places around the world
+fav_places = ['Paris', 'New York', 'Switzerland', 'Italy', 'Maldives']
+
+print("Original order:")
+print(fav_places)
+
+print("\nAlphabetical:")
+print(sorted(fav_places))
+
+print("\nOriginal order:")
+print(fav_places)
+
+print("\nReverse alphabetical:")
+print(sorted(fav_places, reverse=True))
+
+print("\nOriginal order:")
+print(fav_places)
+
+print("\nReversed:")
+fav_places.reverse()
+print(fav_places)
+
+print("\nOriginal order:")
+fav_places.reverse()
+print(fav_places)
+
+print("\nAlphabetical")
+fav_places.sort()
+print(fav_places)
+
+print("\nReverse alphabetical")
+fav_places.sort(reverse=True)
+print(fav_places)
