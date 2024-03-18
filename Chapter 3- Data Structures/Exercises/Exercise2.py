dea3ea9ec2474acf433b564a085c2f1c9f19c195
avacinth <toribiohyacinth05@gmail.com>
2024-03-19 00:26:55 +0400

    Update Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,17 @@
+# Create a List
+
+friend_name = ["Jennie", "Rose", "Jisoo", "Lisa"]
+
+# Accessing Elements
+
+msg = "You look so beautiful today, " + friend_name[0]
+print(msg)
+
+msg = "You look so beautiful today, " + friend_name[1]
+print(msg)
+
+msg = "You look so beautiful today, " + friend_name[2]
+print(msg)
+
+msg = "You look so beautiful today, " + friend_name[3]
+print(msg)
