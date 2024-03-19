2fe4b58c4d9f81cb1476f8aa6efa7b075489c4b0
avacinth <toribiohyacinth05@gmail.com>
2024-03-19 23:53:35 +0400

    Update Exercise5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise5.py
@@ -0,0 +1,16 @@
+# Create a list
+
+favorite_fruits = ['strawberry', 'watermelon', 'orange']
+
+# If statement
+
+if 'watermelon' in favorite_fruits:
+    print ('You really like watermelon!')
+if 'pineapple' in favorite_fruits:
+    print ('You really like pineapple!')
+if 'melon' in favorite_fruits:
+    print ('You really like melon!')
+if 'strawberry' in favorite_fruits:
+    print ('You really like strawberry!')
+if 'apple' in favorite_fruits:
+    print ('You really like apple!')
