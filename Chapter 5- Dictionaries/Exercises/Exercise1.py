c65e7cf6a3e7de2d0741bc260ff76cd22d006c6b
avacinth <toribiohyacinth05@gmail.com>
2024-03-19 23:54:23 +0400

    Update Exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -0,0 +1,15 @@
+# Dictionaries about a person
+
+prsn = {
+    'f_name': 'Hyacinth',
+    'l_name': 'Toribio',
+    'age': '19',
+    'city': 'Pampanga'
+}
+
+# Printing the values of the Dictionaries
+
+print(prsn['f_name'])
+print(prsn['l_name'])
+print(prsn['age'])
+print(prsn['city'])
