65eef48a35dae05d5d179c46e6effb44e8a37172
avacinth <toribiohyacinth05@gmail.com>
2024-03-28 20:53:51 +0400

    Update Exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -0,0 +1,26 @@
+# Dictionaries
+
+glossary = {
+    'Variables': 'It is a container where data values are stored.',
+    'String': 'Is a sequence of characters enclosed within quotes.',
+    'List': 'Is a mutable and ordered collection of items inside a square brackets.',
+    'Integer': 'Is a data type that represents a whole number and not a fractional number.',
+    'Float': 'Is a data type that represents a number with decimal points.',
+}
+
+# String concatenation
+
+terms = 'Variables'
+print(terms + ": " + glossary[terms])
+
+terms = 'String'
+print(terms + ": " + glossary[terms])
+
+terms = 'List'
+print(terms + ": " + glossary[terms])
+
+terms = 'Integer'
+print(terms + ": " + glossary[terms])
+
+terms = 'Float'
+print(terms + ": " + glossary[terms])
