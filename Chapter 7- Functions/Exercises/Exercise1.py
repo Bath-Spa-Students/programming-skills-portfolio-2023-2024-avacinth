e935fa6b1420144d800fca49b005b40a0795d22e
avacinth <toribiohyacinth05@gmail.com>
2024-03-29 11:43:04 +0400

    Update Exercise1.py

---
+++ Chapter 7- Functions/Exercises/Exercise1.py
@@ -0,0 +1,7 @@
+def display_message():
+    '''Printing a message about what is being learned in this chapter.'''
+    print("In this chapter, I learned that functions run only when they are called.")
+
+# Calling the display_message function
+    
+display_message()
