de9ef98edbf51815ef27a0253dc4dd18693b7f33
avacinth <toribiohyacinth05@gmail.com>
2024-03-30 02:07:00 +0400

    Create Practice4.py

---
+++ Chapter 3- Data Structures/Practices/Practice4.py
@@ -0,0 +1,8 @@
+numbers1 = [i for i in range(100)]  #List with 100 elements
+numbers2 = []  # Empty list
+
+# Copying the values from numbers1 to number2 using the copy() method
+numbers2 = numbers1.copy()
+
+# Showing that elements of numbers1 was copied to numbers2
+print(numbers2)
