9a474072c12fc0554de8a867d725f333bb74122f
avacinth <toribiohyacinth05@gmail.com>
2024-03-29 11:56:16 +0400

    Update Exercise3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,9 @@
+def make_shirt(size, message):
+    '''Printing a sentence about the size of the shirt and message printed on it.'''
+    print(f"Making a shirt of size {size} with the message: '{message}'.")
+
+# Calling the make_shirt function
+    
+make_shirt("Small", "First time on the Earth!")
+
+make_shirt(size = "Large", message = "It's me, hi!")
