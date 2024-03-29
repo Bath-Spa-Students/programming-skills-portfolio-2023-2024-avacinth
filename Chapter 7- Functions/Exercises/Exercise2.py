b257032f5b13a3953a77f03f9b065bf5ab8fb2a2
avacinth <toribiohyacinth05@gmail.com>
2024-03-29 11:56:10 +0400

    Update Exercise2.py

---
+++ Chapter 7- Functions/Exercises/Exercise2.py
@@ -0,0 +1,7 @@
+def favorite_book(title):
+    '''Printing a message about my favorite book.'''
+    print(f"One of my favorite books is {title}.")
+
+# Calling the favorite_bbook function with the book title
+    
+favorite_book("Better Than The Movies")
