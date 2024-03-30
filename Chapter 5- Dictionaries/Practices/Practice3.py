c2aca4cacabee43c7a3f125b2f1f1a39a4d34c7b
avacinth <toribiohyacinth05@gmail.com>
2024-03-30 19:46:47 +0400

    Create Practice3.py

---
+++ Chapter 5- Dictionaries/Practices/Practice3.py
@@ -0,0 +1,12 @@
+# Dictionary to store information about myself
+
+me = {
+    'Name': 'Hyacinth',
+    'Age': '19',
+    'City': 'Pampanga',
+    'Course': 'Cyber Security'
+}
+
+# Iterate through the values and print
+for value in me.values():
+    print(value)
