8c3cb5ea286e737b5fc84115a0358b6aac6e5c97
avacinth <toribiohyacinth05@gmail.com>
2024-03-30 19:03:24 +0400

    Create Practice4.py

---
+++ Chapter 4- Control Flow/Practices/Practice4.py
@@ -0,0 +1,6 @@
+speed = 35  # Example value for speed that is between 24 to 56
+
+if 24 <= speed <=56:
+    print("Speed is normal")  #If speed is between 24 and 56
+else:
+    print("Speed is abnormal")  #If speed is over 56
