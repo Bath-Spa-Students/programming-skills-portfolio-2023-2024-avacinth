418e27144f9018657d9deaa0a1868fc295b57fe6
avacinth <toribiohyacinth05@gmail.com>
2024-03-30 19:46:49 +0400

    Create Practice4.py

---
+++ Chapter 5- Dictionaries/Practices/Practice4.py
@@ -0,0 +1,12 @@
+# Dictionary to store information about myself
+
+me = {
+    'Name': 'Hyacinth',
+    'Age': '19',
+    'City': 'Pampanga',
+    'Course': 'Cyber Security'
+}
+
+# Iterate through the keys and values and print
+for key, value in me.items():
+    print(f"Key: {key} and Value: {value}")
