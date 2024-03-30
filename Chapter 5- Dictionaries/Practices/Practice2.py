35149e4143b9df75885ef171a5b587bdd65f7419
avacinth <toribiohyacinth05@gmail.com>
2024-03-30 19:46:43 +0400

    Create Practice2.py

---
+++ Chapter 5- Dictionaries/Practices/Practice2.py
@@ -0,0 +1,12 @@
+# Dictionary to store information about myself
+
+me = {
+    'Name': 'Hyacinth',
+    'Age': '19',
+    'City': 'Pampanga',
+    'Course': 'Cyber Security'
+}
+
+# Iterate through the keys and print
+for key in me.keys():
+    print(key)
