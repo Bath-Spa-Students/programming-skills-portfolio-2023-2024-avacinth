509be3496c5f6b55df6c5ea5b4af191b5a9e7b78
avacinth <toribiohyacinth05@gmail.com>
2024-03-30 19:46:51 +0400

    Create Practice5.py

---
+++ Chapter 5- Dictionaries/Practices/Practice5.py
@@ -0,0 +1,10 @@
+# Two dictionaries
+dict1 = {'Name': 'Hyacinth', 
+         'Age': '19'}
+dict2 = {'City': 'Pampanga', 
+         'Course': 'Cyber Security'}
+
+# Merging dictionaries
+merge_dict = {**dict1, **dict2}
+
+print(merge_dict)
